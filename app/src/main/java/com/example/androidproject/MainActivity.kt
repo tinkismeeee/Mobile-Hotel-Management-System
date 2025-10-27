752203771eef7c78e12332dac7ebf798287e5d73
tinkismeeee <tinhisme1@gmail.com>
2025-10-28 00:31:25 +0700

    add sign in screen

--- app/src/main/java/com/example/androidproject/MainActivity.kt
+++ app/src/main/java/com/example/androidproject/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.androidproject
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,10 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val test : TextView = findViewById(R.id.forgotPassword)
+        test.setOnClickListener {
+            test.text = "Clicked"
         }
     }
 }
